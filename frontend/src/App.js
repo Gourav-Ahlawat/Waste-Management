dbea6e3dcc23b9981d0b7f52f97fbbb75c9e50d2
Shaikh1317 <baba14068@gmail.com>
2024-07-06 18:13:59 +0530

    created driver login basic page and installed react icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import { Login } from "./components/export";
+import DriverLogin from "./components/DriverLogin";
+//import { Login } from "./components/export";
 function App() {
   return (
     <div className=" flex w-full h-screen">
       <div className="flex w-full items-center justify-center">
-        <Login />
+        {/* <Login /> */}
+        <DriverLogin />
       </div>
     </div>
   );
