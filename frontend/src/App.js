e1c57c25e4ca5f6623528271b0d7e739f2227abf
Gourav Ahlawat <cse210001019@gmail.com>
2024-07-08 21:25:56 +0530

    Revert "created driver login page"
    This reverts commit e8eb5bcffef90d31a4452da4dbd43a76a36b50f0.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className=" flex w-full h-screen">
       <div className="flex w-full items-center justify-center">
+        {/* <Login /> */}
         <DriverLogin />
       </div>
     </div>
